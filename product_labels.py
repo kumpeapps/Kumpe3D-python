5b668e88a39c4e422f710afce550f19d3d6215c0
Justin Kumpe <jakumpe@kumpes.com>
2024-01-21 20:05:31 -0600

    -

---
+++ product_labels.py
@@ -0,0 +1,27 @@
+import asyncio
+from pyppeteer import launch
+
+
+async def generate_pdf(url, pdf_path):
+    browser = await launch()
+    page = await browser.newPage()
+
+    await page.goto(url)
+
+    await page.pdf({"path": pdf_path, "width": "3.15in", "height": "1.97in"})
+
+    await browser.close()
+
+
+# Run the function
+def generate_label(sku: str):
+    asyncio.get_event_loop().run_until_complete(
+        generate_pdf(
+            "https://www.kumpe3d.com/product_labels.php?sku=" + sku,
+            "label.pdf",
+        )
+    )
+
+
+if __name__ == "__main__":
+    generate_label("ALO-POO-LSN-L01")
