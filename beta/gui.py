592bd171268cfb97775adf478ae69978568b541f
Justin Kumpe <jakumpe@kumpes.com>
2024-01-25 20:43:55 -0600

    -

--- beta/gui.py
+++ beta/gui.py
@@ -4,7 +4,32 @@ def main(page: ft.Page):
     t = ft.Text(value="Hello, world!", color="green")
     page.controls.append(t)
     page.update()
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
+    btn = ft.ElevatedButton("Click me!")
+    page.add(btn)
 
+
+# "Add to Stock",
+#             "Add Filament Roll",
+#             "Open Filament Roll",
+#             "Empty Filament Roll",
+#             "Production Queue",
+#             "Add to Stock & Print Label",
+#             "Print Product Label",
+#             "Print Filament Colors Card",
 def launch():
     ft.app(target=main)
 
