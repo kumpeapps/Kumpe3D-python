e19e1c47912df3d96b20b3d3e67cfebb150c3bb6
Justin Kumpe <jakumpe@kumpes.com>
2024-01-30 19:59:22 -0600

    -

--- debian_update.sh
+++ debian_update.sh
@@ -3,15 +3,20 @@
     if [ ! -f .cur_version ]; then
     echo "0" > .cur_version
     fi
+    echo 5
+    cd /home/kiosk/Kumpe3D-python && git update
+    echo 10
     version=$(<.version)
     cur_version=$(<.cur_version)
-    echo 10
+    echo 20
+    cd /home/kiosk/Kumpe3D-python && git update
+    echo 
 
     if [ $cur_version < 1.2]; then
         sudo apt-get update
-        echo 20
-        sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y
         echo 30
+        sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y
+        echo 40
     fi
 
     echo 98
