c174e49f027dd1f414f5231f84ef1ce4052a9271
Justin Kumpe <jakumpe@kumpes.com>
2024-01-30 20:19:47 -0600

    -

--- debian_update.sh
+++ debian_update.sh
@@ -4,19 +4,17 @@
     echo "0" > .cur_version
     fi
     echo 5
-    cd /home/kiosk/Kumpe3D-python && git pull
+    cd /home/kiosk/Kumpe3D-python && git pull 2>/dev/null
     echo 10
     version=$(cat .version)
     cur_version=$(cat .cur_version)
     echo 20
-    cd /home/kiosk/Kumpe3D-python && git update
     echo 
 
     if [ "`echo "${cur_version} < 1.2" | bc`" -eq 1 ]; then
-        sudo apt-get update
-        touch runupdate
+        sudo apt-get update 2>/dev/null
         echo 30
-        sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y
+        sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y 2>/dev/null
         echo 40
     fi
 
