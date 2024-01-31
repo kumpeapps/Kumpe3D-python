4d4d8f8cd6f05a77110a455daad9b320eee23af6
Justin Kumpe <jakumpe@kumpes.com>
2024-01-30 20:14:50 -0600

    -

--- debian_update.sh
+++ debian_update.sh
@@ -14,6 +14,7 @@
 
     if [ $cur_version < 1.2]; then
         sudo apt-get update
+        touch runupdate
         echo 30
         sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y
         echo 40
