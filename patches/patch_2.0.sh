d4dedaaab0c8448848ce856c1e0dde706d64243a
Justin Kumpe <jakumpe@kumpes.com>
2024-02-02 11:42:48 -0600

    -

--- patches/patch_2.0.sh
+++ patches/patch_2.0.sh
@@ -3,5 +3,9 @@ if [ ! -f /home/kiosk/Kumpe3D-python/patches/installed.2.0 ]; then
         gawk -i inplace '!/USERID/' /home/kiosk/Kumpe3D-python/.env
         echo "USERID=none" >> /home/kiosk/Kumpe3D-python/.env
     fi
+    gawk -i inplace '!/ratpoison/' /home/kiosk/.xinitrc
+    gawk -i inplace '!/xsetroot/' /home/kiosk/.xinitrc
+    echo "xsetroot -solid white -cursor_name left_ptr" >> /home/kiosk/.xinitrc
+    edho "exec ratpoison& flet run" >> /home/kiosk/.xinitrc
     touch /home/kiosk/Kumpe3D-python/patches/installed.2.0
 fi
