fb079cf37734f7fa4f14a9c71b20c55271dbe33f
Justin Kumpe <jakumpe@kumpes.com>
2024-02-02 16:45:23 -0600

    -

--- patches/patch_1.2.1.sh
+++ patches/patch_1.2.1.sh
@@ -1,4 +1,8 @@
 if [ ! -f /home/kiosk/Kumpe3D-python/patches/installed.1.2.1 ]; then
+    if [ ! -f /home/kiosk/terminalmode ]; then
+        touch /home/kiosk/restoreguimode
+        sh /home/kiosk/Kumpe3D-python/bash_scripts/disableGUI.sh
+    fi
     {
         sudo apt-get update
         echo 42
