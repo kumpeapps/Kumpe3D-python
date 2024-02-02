e3b3b60f57fb892326cb1f6ebbed2554eab06d96
Justin Kumpe <jakumpe@kumpes.com>
2024-02-01 20:47:03 -0600

    -

--- filament_colors_card.py
+++ filament_colors_card.py
@@ -1,24 +1,31 @@
+"""Module for printing filament card labels"""
 import setup  # pylint: disable=unused-import, wrong-import-order
 import os
 import asyncio
 import easygui
 from beepy import beep
-from pyppeteer import launch
+import pdfkit
 
 
 async def generate_pdf(url, pdf_path):
-    browser = await launch(options={"args": ["--no-sandbox"]})
-    page = await browser.newPage()
-
-    await page.goto(url)
-
-    await page.pdf({"path": pdf_path, "width": "3.15in", "height": "1.97in"})
-
-    await browser.close()
+    """Generate PDF from URL"""
+    options = {
+        "page-width": "3.15in",
+        "page-height": "1.97in",
+        "margin-top": "0in",
+        "margin-bottom": "0in",
+        "margin-left": "0in",
+        "margin-right": "0in",
+        "print-media-type": "",
+        "no-outline": "",
+        "disable-smart-shrinking": "",
+    }
+    pdfkit.from_url(url, pdf_path, options=options)
 
 
 # Run the function
 def generate_label(sku: str):
+    """Generate PDF Label"""
     asyncio.get_event_loop().run_until_complete(
         generate_pdf(
             "https://www.kumpe3d.com/filament_colors_card.php?color_id=" + sku,
@@ -28,6 +35,7 @@ def generate_label(sku: str):
 
 
 def print_label():
+    """Print The Label"""
     os.system(
         "lp -d Product_Label_Printer -o media=50x80mm -o orientation-requested=4 label.pdf"
     )
