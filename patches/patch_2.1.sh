731c5a5c39e7c51d9c304da67e5d0fae042b1da0
Justin Kumpe <jakumpe@kumpes.com>
2024-02-24 18:13:01 -0600

    -

--- patches/patch_2.1.sh
+++ patches/patch_2.1.sh
@@ -3,6 +3,6 @@ if [ ! -f /home/kiosk/Kumpe3D-python/patches/installed.2.1 ]; then
         touch /home/kiosk/restoreguimode
         sh /home/kiosk/Kumpe3D-python/bash_scripts/disableGUI.sh
     fi
-    pip install flet_easy -y
+    pip3 install -r /home/kiosk/Kumpe3D-python/requirements.txt
     touch /home/kiosk/Kumpe3D-python/patches/installed.2.1
 fi
