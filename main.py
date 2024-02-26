19d1e23f7e16882cb4a53efb57fd7eb0ea44ca2f
Justin Kumpe <jakumpe@kumpes.com>
2024-02-26 16:36:04 -0600

    -

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from views.emptyroll import emptyroll
 from views.productionq import productionq
 from views.productlabel import printproductlabel
 from core.config import ConfigApp
-from core.params import Params
+from core.params import Params as params
 
 app = fs.FletEasy(route_init="/login", route_login="/login")
 
@@ -33,10 +33,39 @@ def login_x(page: ft.Page):
         dlg.open = True
         page.update()
 
-    if not Params.Access.basic:
+    if not params.Access.basic:
         open_dlg()
         return False
 
+    match (
+        page.title,
+        params.Access.basic,
+        params.Access.production,
+        params.Access.orders,
+        params.Access.print_labels,
+        params.Access.filament_stock,
+        params.Access.admin,
+    ):
+        case (_, _, _, _, _, _, True):
+            return True
+        case ("Add Filament Roll", True, _, _, _, _, _):
+            return True
+        case ("Empty Filament Roll", True, _, _, _, True, _):
+            return True
+        case ("Open Filament Roll", True, _, _, _, True, _):
+            return True
+        case ("Add To Stock", True, True, _, _, _, _):
+            return True
+        case ("Production Queue", True, True, _, _, _, _):
+            return True
+        case ("Production Queue", True, _, True, _, _, _):
+            return True
+        case ("Add to Stock & Print Label", True, True, _, True, _, _):
+            return True
+        case ("Print Product Label", True, _, _, True, _, _):
+            return True
+        case ("Print Filament Colors Card", True, _, _, True, _, True):
+            return True
     return False
 
 
@@ -44,6 +73,7 @@ app.add_pages(
     [login, addstock, addroll, openroll, emptyroll, productionq, printproductlabel]
 )
 ConfigApp(app)
+
 
 # We run the application
 app.run()
